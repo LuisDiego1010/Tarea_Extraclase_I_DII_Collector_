cf1f971a137c91cb6bd6d462eec492b004bd9040
LuisDiego <guisogarcia1010@gmail.com>
2021-03-11 18:13:41 -0600

    Make documentation

--- main.cpp
+++ main.cpp
@@ -2,25 +2,29 @@
 
 using namespace std;
 
-
+/*
+ * Class node
+ * Cotains methods and atributtes for create a List and Collector
+ */
 class Node {
 public:
-    //crear constructor
+    //Class atributtes
     int *value= nullptr;
     Node *next = nullptr;
-    Node(int _value){
+    Node(int _value){ //Class Builder
         value=new(int);
         setValue(_value);
     }
-
+    //Declare methods
     void setValue(int _value);
     int getValue();
     void setNext(Node *_next);
     Node* getNext();
+    //Overload methods new and delete
     void* operator new (size_t nodeNew);
     void operator delete (void* nodeDel);
 };
-
+//Class methods
 void Node::setValue(int _value) {
     *value=_value;
 }
@@ -36,15 +40,21 @@ void Node::setNext(Node *_next) {
 Node* Node::getNext() {
     return next;
 }
-
+/*
+ * Class List
+ * It is responsible for storing integer data, it contains its attributes to be able to access and modify this data.
+ */
 class List {
 public:
+    //Class atributtes
     Node *firts=nullptr;
+    //Declare class methods
     void deleteElement(int _value);
-    void showElement();
+    void showElements();
     void insert(int _value);
 };
 
+//Class methods
 void List::insert(int _value) {
     Node *newValue=new Node(_value);
     newValue->next=firts;
@@ -73,7 +83,7 @@ void List::deleteElement(int _value) {
     }
 }
 
-void List::showElement() {
+void List::showElements() {
     int i=0;
     if (firts==nullptr){
         cout<<"The list is empty"<<endl;
@@ -86,16 +96,24 @@ void List::showElement() {
     }
 }
 
+/*
+ * Class Collector
+ * The one responsible for recycling the freed memory in the list class
+ */
 
 class Collector {
 public:
+    //Class atributtes
     static Node *firts;
+    //Declare class methods
     static Node* getFirts();
     static long isEmpty();
     static void insertCollector(Node* _nodeC);
     static void showCollector();
 };
 Node* Collector::firts= nullptr;
+
+//Class methods
 
 Node * Collector::getFirts() {
     Node *temp = firts;
@@ -120,12 +138,13 @@ void Collector::showCollector() {
     }
     Node *temp=firts;
     while (temp!= nullptr){
-        cout<<"("<<i<<")->"<<*temp->value<<endl;
+        cout<<"("<<i<<")->"<<temp->getValue()<<endl;
         temp=temp->getNext();
         i++;
     }
 }
 
+//Overload methods new and delete
 void* Node::operator new(size_t nodeNew){
     //Lógica del new
     if (Collector::isEmpty()==NULL){
@@ -135,21 +154,24 @@ void* Node::operator new(size_t nodeNew){
     }
 }
 
-
 void Node::operator delete(void* nodeDel){
    Collector::insertCollector(static_cast<Node *>(nodeDel));
 }
 
+//Run the program
 int main() {
     List *List1 = new List();
     List1->insert(30);
-    List1->showElement();
+    List1->showElements();
     List1->deleteElement(30);
-    List1->showElement();
+    List1->showElements();
     Collector::showCollector();
     List1->insert(10);
-    List1->showElement();
+    List1->showElements();
     List1->deleteElement(10);
     Collector::showCollector();
+    List1->showElements();
 
+
+    return 0;
 }
